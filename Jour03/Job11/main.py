a9aca390b436cb01719c2d03a3fb38d68a0e4ca4
Martinie de Maisonneuve <lucas-martinie-de-maisonneuve@laplateforme.io>
2023-11-22 16:13:41 +0100

    Update main.py

--- Jour03/Job11/main.py
+++ Jour03/Job11/main.py
@@ -1,8 +1,10 @@
 def time_to_text(min):
-      
-    h = min // 60
-    min = min % 60
-    print (f"{h} heures et {min} minutes")
+    if min % 1 ==0:
+        h = min // 60
+        min = min % 60
+        print (f"{h} heures et {min} minutes")
+    else:
+        print ("Il faut un nombre entier de minutes")
 
 time_to_text(240)
 time_to_text(222)
