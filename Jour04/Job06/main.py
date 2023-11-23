b6ba29dcffbf85e859216c5acb9942972a1e3b59
Martinie de Maisonneuve <lucas.martinie-de-maisonneuve@laplateforme.io>
2023-11-23 10:43:30 +0100

    Job06

--- Jour04/Job06/main.py
+++ Jour04/Job06/main.py
@@ -1,6 +1,3 @@
-# Écrire un programme qui échange les valeurs de la première et de la dernière case
-# d’une liste quelconque non vide.
-
 liste = [12, 45, 61, 87, 99]
 
 liste[0], liste[-1] = liste[-1], liste[0]
