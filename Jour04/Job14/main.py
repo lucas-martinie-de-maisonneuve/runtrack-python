a72ad378d62fca914a9a99c9ddfff577df4d75e5
Martinie de Maisonneuve <lucas.martinie-de-maisonneuve@laplateforme.io>
2023-11-23 13:44:25 +0100

    Job14

--- Jour04/Job14/main.py
+++ Jour04/Job14/main.py
@@ -15,14 +15,7 @@ def my_long_word(num, phrase):
                 tri += mot + " "
 
             mot = ""
-
-    j = 0
-    for n in mot:
-        j += 1
-
-    if j > num:
-        tri += mot
-
+            
     return tri
 
 print(my_long_word(3, "La peur est le chemin vers le côté obscur, la peur mène à la colère, la colère mène à la haine, la haine mène à la souffrance"))
